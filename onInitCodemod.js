4a64b462bd4d6d8cf02d59d486a90a9ba0bb32bd
Shoul Manevitz <shoulma@gmail.com>
2021-07-06 20:18:07 +0300

    Add files via upload

---
+++ onInitCodemod.js
@@ -0,0 +1,83 @@
+export default function transformer(file, api) {
+    const j = api.jscodeshift;
+    const ngLifeCycleHooks = ['$onChanges', '$doCheck', '$onDestroy', '$postLink'];
+
+    j.registerMethods({
+        doesControllerHaveOnInitHook: function() {
+            return this.find(j.FunctionExpression)
+                .closest(j.AssignmentExpression)
+                .filter((path) => path.node.left.property.name === "$onInit").length
+        },
+        findNgLifeCycleHookFunctions: function() {
+            return this.find(j.MemberExpression, (node) => ngLifeCycleHooks.indexOf(node.property.name) > -1)
+                .closest(j.ExpressionStatement);
+        },
+        findThisClosureVariableDeclerations: function(controllerFunctionPath) {
+            return this.find(j.VariableDeclarator, (node) => node.init.type === j.ThisExpression.name)
+                .filter((path) => path.parent.parent.parent.node === controllerFunctionPath.node)
+                .closest(j.VariableDeclaration);
+        }
+    });
+
+    const root = j(file.source);
+
+    //finds controllers defined as part of component
+    let componentInlineControllers = root
+        .find(j.CallExpression, (node) => node.callee.object.name === 'app' &&
+            node.callee.property.name === 'component')
+        .find(j.ObjectExpression)
+        .find(j.FunctionExpression)
+        .filter((path) => {
+            let propertyParentNode;
+            if (path.parent.node.type === j.Property.name){
+                propertyParentNode = path.parent.node;
+            }
+            else if (path.parent.parent.node.type === j.Property.name){
+                propertyParentNode = path.parent.parent.node;
+            }
+            else{
+                return false;
+            }
+            return propertyParentNode.key.name === 'controller';
+        });
+
+    //find all stand alone controllers
+    let controllers = root
+        .find(j.CallExpression)
+        .find(j.FunctionExpression)
+        .filter((path) => {
+            let callExpressionParentNode;
+            if (path.parent.parent.node.type === j.CallExpression.name) {
+                callExpressionParentNode = path.parent.parent.node;
+            }
+            else if (path.parent.node.type === j.CallExpression.name) {
+                callExpressionParentNode = path.parent.node;
+            }
+            else {
+                return false;
+            }
+            return callExpressionParentNode.callee.object.name === "app" &&
+                callExpressionParentNode.callee.property.name === "controller"
+        });
+
+    let allControllers = j(controllers.paths().concat(componentInlineControllers.paths()));
+
+    allControllers
+        .forEach((controllerFunctionPath) => {
+            if (j(controllerFunctionPath).doesControllerHaveOnInitHook()) {
+                return;
+            }
+            let thisClosureDeclerators = j(controllerFunctionPath).findThisClosureVariableDeclerations(controllerFunctionPath);
+            //find all existing life cycle functions so that they can be excluded from the $onInit function scope
+            let lifeCycleFunctions = j(controllerFunctionPath).findNgLifeCycleHookFunctions();
+            let onInitFunction = j.template.expression`this.$onInit = function(){${controllerFunctionPath.node.body}}`;
+            onInitFunction = j.expressionStatement(onInitFunction);
+            let newBlock = j.blockStatement([thisClosureDeclerators.nodes(), onInitFunction, lifeCycleFunctions.nodes()].flat());
+            j(controllerFunctionPath).replaceWith(j.functionExpression(null, controllerFunctionPath.node.params, newBlock));
+            //remove existing life cycle functions so that we don't have duplicates
+            lifeCycleFunctions.remove();
+            thisClosureDeclerators.remove();
+        });
+
+    return root.toSource();
+}
